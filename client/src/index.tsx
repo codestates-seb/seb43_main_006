db758f9c26f46e7b68af4f28a90f29ab4e88bcc6
JHJFE <ghwo9611@gmail.com>
2023-05-01 21:07:30 +0900

    client 기본세팅

---
+++ client/src/index.tsx
@@ -0,0 +1,19 @@
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import store from './store/store.tsx';
+
+const rootElement = document.getElementById('root');
+if (rootElement) {
+  const root = ReactDOM.createRoot(rootElement);
+  root.render(
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  );
+} else {
+  console.error('No root');
+}
