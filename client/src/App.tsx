f0cc22aa124965d05a1ea0d734792c94e197e8c5
JHJFE <ghwo9611@gmail.com>
2023-05-03 21:43:00 +0900

    fix : client 기본세팅

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,9 @@
 import './App.css';
-
+import RoutesComponent from './pages/Routes.tsx';
 function App() {
-  return <div className="App"></div>;
+  return <div className="App">
+       <RoutesComponent />
+  </div>;
 }
 
 export default App;
--- client/src/pages/Routes.tsx
+++ client/src/pages/Routes.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
-import Home from './Home/Home';
-import Cart from './Cart/Cart';
-import Login from './Certification/Login';
-import Signup from './Certification/Signup';
-import Mypage from './Mypage/Mypage';
-import Payment from './Payment/Payment';
-import Place from './Place/Place';
-import PaymentConfirm from './Payment/PaymentConfirm';
-import Alcohol from './Alcohol/Alcohol';
-import AlcoholDetail from './Alcohol/AlcoholDetail';
+import Home from './Home/Home.tsx';
+import Cart from './Cart/Cart.tsx';
+import Login from './Certification/Login.tsx';
+import Signup from './Certification/Signup.js';//수정해야함
+import Mypage from './Mypage/Mypage.tsx';
+import Payment from './Payment/Payment.tsx';
+import Place from './Place/Place.tsx';
+import PaymentConfirm from './Payment/PaymentConfirm.tsx';
+import Alcohol from './Alcohol/Alcohol.tsx';
+import AlcoholDetail from './Alcohol/AlcoholDetail.tsx';
 
 const RoutesComponent = () => {
   return (
@@ -28,3 +28,5 @@ const RoutesComponent = () => {
     </Routes>
   );
 };
+
+export default RoutesComponent;
