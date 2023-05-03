733389ecdf5a1a625b8136dc6d2e0908c4599e25
JHJFE <ghwo9611@gmail.com>
2023-05-03 13:24:20 +0900

    client 기본세팅

---
+++ client/src/styles/GlobalStyle.ts
@@ -0,0 +1,10 @@
+import { createGlobalStyle } from 'styled-components';
+import { reset } from 'styled-reset';
+
+export default createGlobalStyle`
+    ${reset}
+    *{
+        box-sizing: border-box;
+    }
+
+`;
---
+++ client/src/types/Types.d.ts
@@ -0,0 +1,8 @@
+declare module 'my-module' {
+    export interface MyType {
+      id: number;
+      name: string;
+    }
+  
+    export function myFunction(): void;
+  }
