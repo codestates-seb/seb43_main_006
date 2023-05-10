a7cf33d9c0cb07d601bb5b05bb678c3c9a040c6e
sineTlsl <limsieun0313@gmail.com>
2023-05-10 14:02:05 +0900

    feat: 주류 리스트 아이템 선택 시 상세페이지 이동하게끔 구현

--- client/src/pages/AlcoholDetail.tsx
+++ client/src/pages/AlcoholDetail.tsx
@@ -1,10 +1,92 @@
 import React from "react";
 import styled from "styled-components";
+import alcohol from "../assets/images/alcohol.jpeg";
 
-const AlcoholDetailContainer = styled.section``;
+// components
+import ReviewRating from "../components/Common/ReviewRating";
+import PriceRegular from "../components/Common/PriceRegular";
+
+const AlcoholDetailContainer = styled.section`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100%;
+  padding: 0 25px;
+  color: ${({ theme }) => theme.colors.fontColor};
+`;
+
+const AlcoholBox = styled.div`
+  display: flex;
+  max-width: ${({ theme }) => theme.widthSize.contentMax};
+  width: 100%;
+
+  .img_box {
+    flex: 0 1 50%;
+    display: flex;
+    justify-content: flex-end;
+  }
+  .buy_box {
+    > .buy_title {
+      font-size: 35px;
+      font-weight: 600;
+    }
+    > .item_review_rating {
+      display: flex;
+      align-items: center;
+      p {
+        margin-top: 5px;
+        font-size: 14px;
+        font-weight: 600;
+      }
+    }
+
+    .price_box {
+      margin-top: 30px;
+      .item_discount_rate {
+        font-weight: 700;
+        font-size: 20px;
+        color: ${({ theme }) => theme.colors.themeColor};
+        padding-top: 10px;
+        margin-right: 10px;
+      }
+      .item_price {
+        font-weight: 700;
+        line-height: 18px;
+        font-size: 18px;
+      }
+    }
+  }
+`;
+
+const ItemBox = styled.div`
+  flex: 0 1 50%;
+  padding: 0 20px;
+`;
 
 const AlcoholDetail = () => {
-  return <AlcoholDetailContainer>AlcoholDetail!!</AlcoholDetailContainer>;
+  return (
+    <AlcoholDetailContainer>
+      <AlcoholBox>
+        <ItemBox>
+          <img src={alcohol} width={"70%"} />
+        </ItemBox>
+        <div className="buy_box">
+          <p className="buy_title">화요 53</p>
+          <div className="item_review_rating">
+            <ReviewRating size={20} reviewRating={4.5} />
+            <p>(12)</p>
+          </div>
+          <div className="price_box">
+            <span className="item_discount_rate">25%</span>
+            <span className="item_price">
+              <PriceRegular price={47900} />원
+            </span>
+          </div>
+        </div>
+      </AlcoholBox>
+    </AlcoholDetailContainer>
+  );
 };
 
 export default AlcoholDetail;
