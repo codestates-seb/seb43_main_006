ba7cdb42be876276ec0bc72091e53bcdc48a134f
sineTlsl <limsieun0313@gmail.com>
2023-05-13 18:29:59 +0900

    refactor: 주류리스트 API 요청 인스턴스화

---
+++ client/src/services/api/alcoholListAPI.ts
@@ -0,0 +1,27 @@
+import axios from "axios";
+
+const API_URL = `${process.env.REACT_APP_API_URL}/items`;
+const headers = {
+  "Content-Type": "application/json",
+  // accept: "text/html; charset=utf-8",
+  "ngrok-skip-browser-warning": "69420", // ngrok cors 에러
+};
+
+export const getItemsList = async (page: number, size: number, sortBy: string, category: string) => {
+  return await axios.get(API_URL, {
+    params:
+      category === "전체"
+        ? {
+            page,
+            size,
+            sortBy,
+          }
+        : {
+            page,
+            size,
+            category,
+            sortBy,
+          },
+    headers,
+  });
+};
