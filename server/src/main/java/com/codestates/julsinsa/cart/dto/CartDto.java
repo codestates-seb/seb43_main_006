03aa5c247d8485d0ab84386de07bb16356e5b209
jy3026 <jy_3026@naver.com>
2023-05-15 10:38:17 +0900

    Feat: 장바구니 추가

---
+++ server/src/main/java/com/codestates/julsinsa/cart/dto/CartDto.java
@@ -0,0 +1,43 @@
+package com.codestates.julsinsa.cart.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
+import java.util.List;
+
+public class CartDto {
+
+    @Getter
+    public static class Post{
+//        @NotNull
+//        private List<ItemCartDto.Post> itemCarts;
+        @Positive
+        private long itemId;
+
+        @Positive
+        private int quantity;
+    }
+
+    @Getter
+    public static class Delete{
+
+        private List<Long> itemIds;
+    }
+
+    @Getter
+    @Setter
+    @AllArgsConstructor
+    public static class Response{
+        private Long cartId;
+
+//        private int totalQuantity;
+//
+//        private int totalPrice;
+
+        private List<ItemCartDto.Response> itemCarts;
+
+    }
+}
