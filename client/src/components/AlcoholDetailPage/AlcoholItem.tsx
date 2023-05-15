4a9e5cdd660f1ae2cbff9cf906f78b946b635e0b
sineTlsl <limsieun0313@gmail.com>
2023-05-15 14:21:46 +0900

    feat: 결제페이지 데이터 전달

--- client/src/components/AlcoholDetailPage/AlcoholItem.tsx
+++ client/src/components/AlcoholDetailPage/AlcoholItem.tsx
@@ -1,13 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
-
+import { useNavigate } from "react-router-dom";
 import alcohol from "../../assets/images/alcohol.png";
 import { AiFillHeart } from "react-icons/ai";
+import { AlcoholData, ItemOreder } from "../../types/AlcholInterfaces";
 
 // components
 // import ReviewRating from "../Common/ReviewRating";
 import ClickFavoriteItem from "../Common/ClickFavoriteItem";
 import PriceRegular from "../Common/PriceRegular";
+import { ButtonDark, ButtonLight } from "../Common/Button";
+import QuantityControl from "./QuantityControl";
+
+interface ItemDatailProps {
+  data: AlcoholData;
+}
 
 const AlcoholItemContainer = styled.div`
   margin-top: 30px;
@@ -23,8 +30,8 @@ const StyledItemImgBox = styled.div`
   padding: 0 20px;
 
   img {
-    height: 100%;
     margin: auto;
+    height: 550px;
     max-width: 100%;
     object-fit: scale-down;
     width: auto;
@@ -59,7 +66,7 @@ const StyledItemBuyBox = styled.div`
     }
   }
   .buy_price {
-    padding-top: 20px;
+    padding: 30px 0;
     font-size: 16px;
 
     > .buy_item_content {
@@ -79,7 +86,22 @@ const StyledItemBuyBox = styled.div`
     padding: 20px;
     border-radius: 6px;
     border: 1px solid lightgray;
-    margin: 20px 0;
+    margin-bottom: 20px;
+
+    p {
+      font-size: 13px;
+      margin-bottom: 12px;
+    }
+    .quantity_box {
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+
+      .item_price {
+        font-size: 1.5rem;
+        font-weight: 700;
+      }
+    }
   }
 
   .buy_cart {
@@ -108,48 +130,66 @@ const StyledItemBuyBox = styled.div`
   }
 `;
 
-const AlcoholItem = () => {
+const AlcoholItem = ({ data }: ItemDatailProps) => {
+  const [quantity, setQuantity] = useState<number>(1);
+
+  const handleQuantityChange = (newQuantity: number) => {
+    setQuantity(newQuantity);
+  };
+
+  const navigate = useNavigate();
+  // const HandlerClickCart = () => {};
+  const HandlerClickhistory = (): void => {
+    const props: ItemOreder = {
+      itemId: data.itemId,
+      titleKor: data.titleKor,
+      price: data.price * quantity,
+      capacity: data.capacity,
+      quantity,
+    };
+    navigate("/payment", {
+      state: props,
+    });
+  };
+
   return (
     <AlcoholItemContainer>
       <StyledItemImgBox>
-        <img src={alcohol} />
+        <img src={data.profile} />
       </StyledItemImgBox>
       <StyledItemBuyBox>
         <div className="buy_titlebox">
-          <p className="buy_title">화요 53</p>
+          <p className="buy_title">{data.titleKor}</p>
           <div className="item_like">
             <ClickFavoriteItem icon={AiFillHeart} color="#e4e5e9" activeColor="#D43635" size={30} />
           </div>
         </div>
-        {/* 별점 (별점 갯수) */}
-        {/* <div className="item_review_rating">
-            <ReviewRating size={20} reviewRating={4.5} />
-            <p>(12)</p>
-          </div> */}
         <div className="buy_price">
           <div className="buy_item_content">
             <span className="buy_item_content_title">판매가</span>
             <span className="buy_price_text">
-              <PriceRegular price={47900} />원
+              <PriceRegular price={data.price} />원
             </span>
           </div>
           <div className="buy_item_content">
             <span className="buy_item_content_title">용량</span>
-            <span>125ml</span>
+            <span>{data.capacity}ml</span>
           </div>
         </div>
         <div className="buy_count">
-          <p>현재 남은 수량 중 최대 10개 구매 가능</p>
-          <div>
-            <div>상품 수량 칸</div>
-            <div style={{ fontSize: "20px", fontWeight: "bold" }}>
-              <PriceRegular price={47900} />원
+          <p>현재 남은 수량 중 최대 {data.quantity}개 구매 가능</p>
+          <div className="quantity_box">
+            <QuantityControl quantity={quantity} maxQuantity={data.quantity} onQuantityChange={handleQuantityChange} />
+            <div className="item_price">
+              <PriceRegular price={data.price * quantity} />원
             </div>
           </div>
         </div>
         <div className="buy_cart">
           <button className="cart_btn">장바구니</button>
-          <button className="buy_btn">구매하기</button>
+          <ButtonDark width="100%" height="100%" fontSize="14px" fontWeight="500" onClick={HandlerClickhistory}>
+            구매하기
+          </ButtonDark>
         </div>
       </StyledItemBuyBox>
     </AlcoholItemContainer>
