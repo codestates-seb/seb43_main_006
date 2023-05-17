d962592b5796ff1962ff859d82835cfb5e80e854
JHJFE <ghwo9611@gmail.com>
2023-05-17 17:36:21 +0900

    feat: craco 설정 파일을 사용해 웹팩 설정 재정의 적용

---
+++ client/craco.config.js
@@ -0,0 +1,16 @@
+// craco.config.js
+
+const TsconfigPathsPlugin = require("tsconfig-paths-webpack-plugin");
+
+module.exports = {
+  plugins: [
+    {
+      plugin: {
+        overrideWebpackConfig: ({ webpackConfig }) => {
+          webpackConfig.resolve.plugins.push(new TsconfigPathsPlugin({}));
+          return webpackConfig;
+        },
+      },
+    },
+  ],
+};
