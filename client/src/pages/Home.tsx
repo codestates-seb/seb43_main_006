a1c6d70040be64e9718e57322556818080eba66d
JHJFE <ghwo9611@gmail.com>
2023-05-19 11:12:41 +0900

    feat: 로그인시 멤버 id 로컬 저장

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -39,13 +39,15 @@ const Home: React.FC = () => {
     const refreshToken = urlParams.get("refresh_token");
     const iatDate = urlParams.get("iat");
     const expDate = urlParams.get("exp");
-    if (accessToken && refreshToken && iatDate && expDate) {
+    const memberId = urlParams.get("X-Member-ID");
+    if (accessToken && refreshToken && iatDate && expDate && memberId) {
       localStorage.setItem("authToken", accessToken.replace(/^Bearer\s/, "")); // 토큰 저장
       localStorage.setItem("refresh", refreshToken); // refresh 토큰 저장
       const iat_sec = convertToSeconds(iatDate);
       const exp_sec = convertToSeconds(expDate);
       localStorage.setItem("exp", exp_sec); // 토큰 만료시간 저장
       localStorage.setItem("iat", iat_sec); // refresh 토큰 생성 시간 저장
+      localStorage.setItem("memberId", memberId); // member id 저장
       navigate("/");
     }
   }, []);
