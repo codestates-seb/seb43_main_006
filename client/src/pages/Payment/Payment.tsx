30ac80bce81c56c38667b8750dd906e1ebf31b55
sunwoo020 <sunwoo020@naver.com>
2023-05-25 23:38:46 +0900

    fix: 아이템리스트->결제페이지 수정

--- client/src/pages/Payment/Payment.tsx
+++ client/src/pages/Payment/Payment.tsx
@@ -12,7 +12,9 @@ import { UserProps } from "types/AlcholInterfaces";
 import useAxiosAll from "@hooks/useAxiosAll";
 import { useSelector } from "react-redux";
 import { stateProps } from "./Paymentpayinfo";
+import axios from "axios";
 import Modal from "@layout/Header/Logoutmodal";
+import { Itemtype } from "@pages/Cart";
 
 function authTokenExpired(authToken: string) {
   if (!authToken) {
@@ -45,10 +47,12 @@ const Payment = () => {
   const selectdata = useSelector((state: stateProps) => state.markerState);
   const [userInfo, setUserInfo] = useState<UserProps>({} as UserProps);
   const [isModalOpen, setIsModalOpen] = useState(false);
+
   const updateUserInfo = () => {
     setUserInfo(data as UserProps);
   };
   const authToken = localStorage.getItem("authToken");
+  const access_token = `Bearer ${authToken}`;
   useEffect(() => {
     // Check if the authToken is missing or expired
     if (!authToken || authTokenExpired(authToken)) {
@@ -68,11 +72,51 @@ const Payment = () => {
   }, [data]);
 
   const [selectedDate, setSelectedDate] = useState<Date | null>(null);
+
   const handlePayment = () => {
     if (!selectedDate || !selectdata || !items) {
       return setIsModalOpen(true);
     }
-    navigate("/CheckoutChang", { state: { items: items, userInfo: userInfo } });
+
+    // 서버에서 중복된 itemId를 가진 데이터 조회
+
+    axios
+      .get(`${process.env.REACT_APP_API_URL}/cart`, {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: access_token,
+          "ngrok-skip-browser-warning": "69420",
+        },
+      })
+      .then((response) => {
+        const { itemCarts } = response.data.data;
+
+        const isDuplicate = itemCarts.some((item: Itemtype) => item.itemId === items[0].itemId);
+
+        if (isDuplicate) {
+          navigate("/CheckoutChang", { state: { items: items, userInfo: userInfo } });
+        } else {
+          const itemOrder = { itemId: items[0].itemId, quantity: items[0].quantity };
+          axios
+            .post(`${process.env.REACT_APP_API_URL}/cart`, itemOrder, {
+              headers: {
+                "Content-Type": "application/json",
+                Authorization: access_token,
+                "ngrok-skip-browser-warning": "69420",
+              },
+            })
+            .then((response) => {
+              console.log("Payment data posted successfully:", response.data);
+              navigate("/CheckoutChang", { state: { items: items, userInfo: userInfo } });
+            })
+            .catch((error) => {
+              console.log("Error during payment data posting:", error);
+            });
+        }
+      })
+      .catch((error) => {
+        console.log("Error during data retrieval:", error);
+      });
   };
 
   const handleDateChange = (date: Date | null) => {
